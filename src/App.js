cbefe2c3e9b2220fc1a8d0f056d4ef39bfe62b4b
Mitch Lubecke <mitch.lubecke@gmail.com>
2024-11-28 03:19:29 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HypnoticIllusion from './components/HypnoticIllusion';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <HypnoticIllusion />
+    </div>
+  );
+}
+
+export default App;
