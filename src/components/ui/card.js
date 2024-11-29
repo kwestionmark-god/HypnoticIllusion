9c71c5f83a7659be054272bad89e43f9ab2980a1
Mitch Lubecke <44012221+kwestionmark-god@users.noreply.github.com>
2024-11-29 02:28:08 -0600

    Updates

--- src/components/ui/card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Card = ({ children }) => {
-  return <div className="card">{children}</div>;
-};
-
-export const CardContent = ({ children }) => {
-  return <div className="card-content">{children}</div>;
-};
